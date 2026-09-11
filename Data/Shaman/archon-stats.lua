08ee2e1fa6ffbad02aec31828c30e6253a7475e6
SynCry <99047432+SynCryTV@users.noreply.github.com>
2026-09-11 22:15:47 +0200

    hhh

--- Data/Shaman/archon-stats.lua
+++ Data/Shaman/archon-stats.lua
@@ -1,10 +1,16 @@
 -- Manuell aktualisiert via Grimoire-scraper (nicht offiziell)
 GrimoireArchonStats = GrimoireArchonStats or {}
 GrimoireArchonStats["SHAMAN"] = {
-  ["elemental"] = {
-  },
   ["enhancement"] = {
+    ["Mythic+"] = { targets = { mastery = 1049, haste = 853, crit = 424, versatility = 144 } },
+    ["Raid"] = { targets = { mastery = 1157, haste = 1010, crit = 354, versatility = 47 } },
+  },
+  ["elemental"] = {
+    ["Mythic+"] = { targets = { mastery = 1007, crit = 733, haste = 552, versatility = 148 } },
+    ["Raid"] = { targets = { mastery = 1163, crit = 722, haste = 587, versatility = 80 } },
   },
   ["restoration"] = {
+    ["Mythic+"] = { targets = { crit = 942, haste = 530, versatility = 435, mastery = 419 } },
+    ["Raid"] = { targets = { crit = 1120, mastery = 607, haste = 412, versatility = 354 } },
   },
 }
